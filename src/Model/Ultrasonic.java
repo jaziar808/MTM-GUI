85b32f5f3a330a9861fd365f79e3d87ff9be7197
Jacob Ziarniak <jaziar808@gmail.com>
2023-03-23 14:29:55 -0400

    Completed skeleton structure for ultrasonic sensor class

--- src/Model/Ultrasonic.java
+++ src/Model/Ultrasonic.java
@@ -5,7 +5,44 @@ import java.util.*;
 
 public class Ultrasonic implements Device {
 
-    /** list of the pin numbers that this ultrasonic sensor has been allocated by the arduino */
-    private List<Integer> pinList;
+    /** list of the pin numbers that this servo motor has been allocated by the arduino */
+    private HashMap<String, Integer> pinNums;
+
+    /**
+     * Used to get the pin numbers that are associated with this ultrasonic sensor
+     * @return number of all arduino pins that this ultrasonic sensor is connected to
+     */
+    @Override
+    public Collection<Integer> getPinNums(){
+        return this.pinNums.values();
+    }
+
+    /**
+     * Used to read data from the ultrasonic sensor
+     * @return UNKOWN HOW THIS WILL RETURN DATA AT THE MOMENT
+     */
+    @Override
+    public List<String> read(){
+        // TODO
+        return null;
+    }
+
+    /**
+     * Write to one of the ultrasonic sensors's pins
+     * @param pinNum the number of the pin that is being written to
+     */
+    @Override
+    public void update(int pinNum){
+        // TODO
+    }
+
+    /**
+     * Allocate pin numbers to this ultrasonic sensor
+     * @param pinNums a map of string keys mapped to integers representing pin numbers
+     */
+    @Override
+    public void allocPins(HashMap<String, Integer> pinNums){
+        this.pinNums = pinNums;
+    }
 
 }
